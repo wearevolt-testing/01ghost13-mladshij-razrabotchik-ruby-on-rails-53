d6d81f76d9fbed307d93e30cab4e31045287e617
01ghost13 <orkich@gmail.com>
2017-10-22 21:10:07 +0300

    another change

--- app/mailers/report_by_author_mailer.rb
+++ app/mailers/report_by_author_mailer.rb
@@ -1,4 +1,5 @@
 class ReportByAuthorMailer < ApplicationMailer
+  default from: ENV['MAILGUN_SMTP_LOGIN']
   def report_by_author(users, email, start_date, finish_date)
     @users = users
     mail(to: email, subject: "Report by author from: #{start_date.to_s} to #{finish_date.to_s}")
