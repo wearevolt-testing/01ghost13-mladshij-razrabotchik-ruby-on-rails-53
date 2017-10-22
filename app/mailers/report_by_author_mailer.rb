8f2ea173828ccc45a2b1e763e6e6da1ed995eafd
01ghost13 <orkich@gmail.com>
2017-10-22 20:03:51 +0300

    changed default mail

--- app/mailers/report_by_author_mailer.rb
+++ app/mailers/report_by_author_mailer.rb
@@ -1,5 +1,4 @@
 class ReportByAuthorMailer < ApplicationMailer
-  default from: 'test@mail.com'
   def report_by_author(users, email, start_date, finish_date)
     @users = users
     mail(to: email, subject: "Report by author from: #{start_date.to_s} to #{finish_date.to_s}")
